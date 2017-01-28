b2ff0e2e2c76e8c146982c16a0ec57ac748074f3
Fox <thehouseofthefox@hotmail.com>
2017-01-28 19:58:30 +0800

    ADC player ready.

--- src/drv_adc.c
+++ src/drv_adc.c
@@ -18,15 +18,26 @@
 #define STATUS_RELEASE	0
 #define STATUS_PRESS	1
 
+#define THREASHOLD_GND		2700	/* ADC value when connected to GND */
+#define THREASHOLD_VALID	3700	/* Valid ADC value when connected to VDD */
+#define FLT_CNT_PRESS		2		/* Press filter counter */
+#define FLT_CNT_RELEASE		2		/* Release filter counter */
+#define FLT_CNT_INIT		0		/* Initial value of counters */
 
 /* ADC device file names */
 static char *_adcChStrP[SYSFS_ADC_DIR_CHMAX] = { NULL };
-
-#define THREASHOLD_GND		2500
-#define THREASHOLD_VALID	4000
-#define FLT_CNT_PRESS		2
-#define FLT_CNT_RELEASE		3
-#define FLT_CNT_INIT		0
+/* ADC channel status: PRESS/RELEASE */
+char adcStatus[SYSFS_ADC_DIR_CHMAX] =
+{ STATUS_RELEASE, STATUS_RELEASE, STATUS_RELEASE,
+	STATUS_RELEASE, STATUS_RELEASE, STATUS_RELEASE };
+/* ADC channel press filter counter */
+char adcPressCnt[SYSFS_ADC_DIR_CHMAX] =
+{ FLT_CNT_INIT, FLT_CNT_INIT, FLT_CNT_INIT,
+	FLT_CNT_INIT, FLT_CNT_INIT, FLT_CNT_INIT };
+/* ADC channel release filter counter */
+char adcReleaseCnt[SYSFS_ADC_DIR_CHMAX] =
+{ FLT_CNT_INIT, FLT_CNT_INIT, FLT_CNT_INIT,
+	FLT_CNT_INIT, FLT_CNT_INIT, FLT_CNT_INIT };
 
 /* Get ADC channel value 0~4095 */
 int adcGetChVal(int ch)
@@ -99,7 +110,6 @@ void adcInit(void)
 	_adcChStrP[5] = SYSFS_ADC_DIR_CH5;
 	_adcChStrP[6] = SYSFS_ADC_DIR_CH6;
 
-
 	fd = fopen(SYSFS_ADC_DIR_CH0, "w");
 	if (fd == NULL) /* Load ADC if it is not loaded*/
 	{
@@ -107,21 +117,11 @@ void adcInit(void)
 		fwrite("BB-ADC", sizeof(int), 6, fd); /* "BB-ADC" has 6 characters */
 		fclose(fd);
 	}
-
 }
 
 void adcTest(void)
 {
-	int x;
-	char adcStatus[SYSFS_ADC_DIR_CHMAX] =
-	{ STATUS_RELEASE, STATUS_RELEASE, STATUS_RELEASE,
-		STATUS_RELEASE, STATUS_RELEASE, STATUS_RELEASE };
-	char adcPressCnt[SYSFS_ADC_DIR_CHMAX] =
-	{ FLT_CNT_INIT, FLT_CNT_INIT, FLT_CNT_INIT,
-		FLT_CNT_INIT, FLT_CNT_INIT, FLT_CNT_INIT };
-	char adcReleaseCnt[SYSFS_ADC_DIR_CHMAX] =
-	{ FLT_CNT_INIT, FLT_CNT_INIT, FLT_CNT_INIT,
-		FLT_CNT_INIT, FLT_CNT_INIT, FLT_CNT_INIT };
+	int adcVal = 0; /* ADC channel value */
 	int adcChIdx = 0;
 
 	adcInit();
@@ -130,24 +130,13 @@ void adcTest(void)
 	{
 		for (adcChIdx = 0; adcChIdx < SYSFS_ADC_DIR_CHMAX; adcChIdx++)
 		{
+			adcVal = adcGetChVal(adcChIdx);
 
-			x = adcGetChVal(adcChIdx);
-//			adc=((float)x/4095.0)*1.8;
-//			printf("[%4f] ",adc);
-			if (x < THREASHOLD_GND)
-			{
-				//printf("------------------------%d:%d \n", adcChIdx, x);
-			}
-			else
-			{
-				//printf("#%d:%d ", adcChIdx, x);
-			}
-//			if (6 == adcChIdx)
-//			{ printf("\n"); }
-
-			if ((x < THREASHOLD_GND) && (adcStatus[adcChIdx] == STATUS_RELEASE))
+			/* Release status + Press detected */
+			if ((adcVal < THREASHOLD_GND) && (adcStatus[adcChIdx] == STATUS_RELEASE))
 			{
 				char *musicP = NULL;
+				/* Press filter */
 				if (FLT_CNT_PRESS > adcPressCnt[adcChIdx])
 				{
 					adcPressCnt[adcChIdx]++;
@@ -156,7 +145,8 @@ void adcTest(void)
 				{
 					adcStatus[adcChIdx] = STATUS_PRESS;
 					adcReleaseCnt[adcChIdx] = FLT_CNT_INIT;
-					printf("[%d] ON[%d]\n", adcChIdx, x);
+					//printf("[%d] ON[%d]\n", adcChIdx, adcVal);
+					/* Play the wav file */
 					switch (adcChIdx)
 					{
 					case 0:
@@ -181,20 +171,23 @@ void adcTest(void)
 						musicP = "./wavPlayer ./Piano/B4.wav &";
 						break;
 					default:
-						printf("Impossible!\n");
+						printf("Fatal Error!\n");
 						break;
 					}
-					printf("playing: %s\n", musicP);
+					//printf("playing: %s\n", musicP);
 					system(musicP);
 				}
 			}
-			if ((x < THREASHOLD_GND) && (adcStatus[adcChIdx] == STATUS_PRESS))
+			/* Press status + Press detected */
+			else if ((adcVal < THREASHOLD_GND) && (adcStatus[adcChIdx] == STATUS_PRESS))
 			{
 				adcReleaseCnt[adcChIdx] = FLT_CNT_INIT;
 			}
-			else if ((x >= THREASHOLD_GND) && (x <= THREASHOLD_VALID)
+			/* Press status + Release detected */
+			else if ((adcVal >= THREASHOLD_GND) && (adcVal <= THREASHOLD_VALID)
 					&& (adcStatus[adcChIdx] == STATUS_PRESS))
 			{
+				/* Release filter */
 				if (FLT_CNT_RELEASE > adcReleaseCnt[adcChIdx])
 				{
 					adcReleaseCnt[adcChIdx]++;
@@ -203,10 +196,11 @@ void adcTest(void)
 				{
 					adcStatus[adcChIdx] = STATUS_RELEASE;
 					adcPressCnt[adcChIdx] = FLT_CNT_INIT;
-					printf("[%d] OFF[%d]\n", adcChIdx, x);
+					//printf("[%d] OFF[%d]\n", adcChIdx, adcVal);
 				}
 			}
-			else if ((x >= THREASHOLD_GND) && (x <= THREASHOLD_VALID)
+			/* Release status + Release detected */
+			else if ((adcVal >= THREASHOLD_GND) && (adcVal <= THREASHOLD_VALID)
 					&& (adcStatus[adcChIdx] == STATUS_RELEASE))
 			{
 				adcPressCnt[adcChIdx] = FLT_CNT_INIT;
@@ -215,127 +209,5 @@ void adcTest(void)
 			usleep(1000);
 		}
 	}
-
 }
-#if 0
-{
-	FILE *fd=NULL;
-	char a[10];
-	int x;
-	float adc;
-	char *_adcChStrP[SYSFS_ADC_DIR_CHMAX] =
-	{	NULL};
-	char adcStatus[SYSFS_ADC_DIR_CHMAX] =
-	{	0,0,0,0,0,0};
-	char adcStatusCnt[SYSFS_ADC_DIR_CHMAX] =
-	{	0,0,0,0,0,0};
-	char adcChIdx = 0;
 
-	_adcChStrP[0] = SYSFS_ADC_DIR_CH0;
-	_adcChStrP[1] = SYSFS_ADC_DIR_CH1;
-	_adcChStrP[2] = SYSFS_ADC_DIR_CH2;
-	_adcChStrP[3] = SYSFS_ADC_DIR_CH3;
-	_adcChStrP[4] = SYSFS_ADC_DIR_CH4;
-	_adcChStrP[5] = SYSFS_ADC_DIR_CH5;
-	_adcChStrP[6] = SYSFS_ADC_DIR_CH6;
-
-	fd=fopen(SYSFS_ADC_DIR_CH0,"w");
-	if (fd==NULL) /* Load ADC if it is not loaded*/
-	{
-		fd=fopen(SYSFS_ADC_DEV,"w");
-		fwrite("BB-ADC",sizeof(int),6,fd); /* "BB-ADC" has 6 characters */
-		fclose(fd);
-	}
-
-	while(1)
-	{
-		for (adcChIdx = 0; adcChIdx < SYSFS_ADC_DIR_CHMAX; adcChIdx++)
-		{
-			fd=fopen(_adcChStrP[adcChIdx],"r+");
-			fscanf(fd,"%s",a);
-//			printf("%s",a);
-			if(strlen(a)==4)
-			{
-				x=((int)a[0]-48)*1000+((int)a[1]-48)*100+((int)a[2]-48)*10+((int)a[3]-48);
-			}
-			else if(strlen(a)==3)
-			{
-				x=((int)a[0]-48)*100+((int)a[1]-48)*10+((int)a[2]-48);
-			}
-			else if(strlen(a)==2)
-			{
-				x=((int)a[0]-48)*10+((int)a[1]-48);
-			}
-			else
-			{
-				x=((int)a[0]-48);
-			}
-//			adc=((float)x/4095.0)*1.8;
-//			printf("[%4f] ",adc);
-			if (x < THREASHOLD_GND)
-			{
-				printf("-%d:%dx \n", adcChIdx, x);
-			}
-			else
-			{
-				//printf("#%d:%dx ", adcChIdx, x);
-			}
-//			if (6 == adcChIdx)
-//			{ printf("\n"); }
-
-			if ((x < THREASHOLD_GND) && (adcStatus[adcChIdx] == 0))
-			{
-				char *musicP = NULL;
-				if (FLT_CNT_PRESS != adcStatusCnt[adcChIdx])
-				{	adcStatusCnt[adcChIdx]++;}
-				if (FLT_CNT_PRESS == adcStatusCnt[adcChIdx])
-				{
-					adcStatus[adcChIdx] = 1;
-					//printf("[%d] ON[%d]\n", adcChIdx, x);
-					switch (adcChIdx)
-					{
-						case 0:
-						musicP = "./wavPlayer ./Piano/C4.wav &";
-						break;
-						case 1:
-						musicP = "./wavPlayer ./--Piano/D4.wav &";
-						break;
-						case 2:
-						musicP = "./wavPlayer ./Piano/E4.wav &";
-						break;
-						case 3:
-						musicP = "./wavPlayer ./Piano/F4.wav &";
-						break;
-						case 4:
-						musicP = "./wavPlayer ./Piano/G4.wav &";
-						break;
-						case 5:
-						musicP = "./wavPlayer ./Piano/A4.wav &";
-						break;
-						case 6:
-						musicP = "./wavPlayer ./Piano/B4.wav &";
-						break;
-						default:
-						break;
-					}
-					system(musicP);
-				}
-			}
-			else if ((x >= THREASHOLD_GND) && (x != 4095) && (adcStatus[adcChIdx] == 1))
-			{
-				if (0 != adcStatusCnt[adcChIdx])
-				{	adcStatusCnt[adcChIdx]--;}
-				if (0 == adcStatusCnt[adcChIdx])
-				{
-					adcStatus[adcChIdx] = 0;
-					//printf("[%d] OFF[%d]\n", adcChIdx, x);
-				}
-			}
-
-			fclose(fd);
-			usleep(1000);
-		}
-		//printf("\n");
-	}
-}
-#endif
